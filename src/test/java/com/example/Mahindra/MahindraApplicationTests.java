6870c59b92fb045a130170fad6a54c4e49f6e42b
ErRaghvendra <mishra.raghav3110@gmail.com>
2025-11-16 15:35:24 +0530

    coderefactor

---
+++ src/test/java/com/example/Mahindra/MahindraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mahindra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahindraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
